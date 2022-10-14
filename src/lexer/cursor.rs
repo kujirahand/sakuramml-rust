df2989b9c5fe9a355eb01c5a810757f267b3aecf
kujirahand <web@kujirahand.com>
2022-10-14 17:08:46 +0900

    fixed test

--- src/lexer/cursor.rs
+++ src/lexer/cursor.rs
@@ -20,6 +20,27 @@ impl TokenCursor {
         self.index += 1;
         Some(c)
     }
+    pub fn peek(&mut self) -> Option<char> {
+        if self.is_eos() { return None; }
+        let c = self.src[self.index];
+        Some(c)
+    }
+    pub fn is_numeric(&self) -> bool {
+        if self.is_eos() { return false; }
+        let c = self.src[self.index];
+        return ('0' <= c) && (c <= '9');
+    }
+    pub fn eq(&self, s: &str) -> bool {
+        let s2: Vec<char> = s.chars().collect();
+        for i in 0..s2.len() {
+            let idx = self.index + i;
+            println!("{}={}", self.src[idx], s2[i]);
+            if self.src[idx] != s2[i] {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 #[cfg(test)]
@@ -31,5 +52,7 @@ mod tests {
         assert_eq!(cur.is_eos(), false);
         assert_eq!(cur.next(), Some('l'));
         assert_eq!(cur.next(), Some('1'));
+        assert_eq!(cur.next(), Some('6'));
+        assert_eq!(cur.eq("cde"), true);
     }
 }
