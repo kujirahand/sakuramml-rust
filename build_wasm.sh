85e993e4d6a79b4f00d6b764e5516ca3221dc362
kujirahand <web@kujirahand.com>
2022-10-19 22:44:59 +0900

    v0.1.6

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+
 wasm-pack build --target web
+
+cd $SCRIPT_DIR/src
+cnako3 batch_extract_command.nako3
 
--- src/sakura_version.rs
+++ src/sakura_version.rs
@@ -1,5 +1,5 @@
 /// Sakura Version
-const SAKURA_VERSION: &str = "0.1.5";
+const SAKURA_VERSION: &str = "0.1.6";
 
 pub fn version_str() -> String {
     SAKURA_VERSION.to_string()
