04efb19f2797426c4fe0830c8049459c5387f7bc
kujirahand <web@kujirahand.com>
2023-08-07 15:47:35 +0900

    Sutotonをサクラv1互換に修正

--- build_doc.sh
+++ build_doc.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+# wasm-pack build --target web
 
-
-wasm-pack build --target web
-
+echo "--- build doc ---"
 cd $SCRIPT_DIR/src
 cnako3 batch_extract_command.nako3
 
--- src/sutoton.rs
+++ src/sutoton.rs
@@ -2,7 +2,7 @@
 use super::cursor::TokenCursor;
 use super::token::zen2han;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct SutotonItem {
     name: String,
     value: String,
@@ -29,10 +29,6 @@ impl SutotonList {
             sorted: false,
         }
     }
-    fn push(&mut self, it: SutotonItem) {
-        self.items.push(it);
-        self.sorted = false;
-    }
     fn sort_items(&mut self) {
         if self.sorted {
             return;
@@ -51,7 +47,8 @@ impl SutotonList {
                 return;
             }
         }
-        self.items.push(SutotonItem::from(name, value));
+        let item = SutotonItem::from(name, value);
+        self.items.push(item);
         self.sorted = false;
     }
 }
@@ -59,80 +56,103 @@ impl SutotonList {
 fn init_items() -> SutotonList {
     let mut items = SutotonList::new();
     // <SUTOTON>
-    items.push(SutotonItem::from("ここから演奏", "?"));
-    items.push(SutotonItem::from("テンポ", "TEMPO="));
-    items.push(SutotonItem::from("トラック", "TR="));
-    items.push(SutotonItem::from("チャンネル", "CH="));
-    items.push(SutotonItem::from("全音符", "l1"));
-    items.push(SutotonItem::from("二分音符", "l2"));
-    items.push(SutotonItem::from("四分音符", "l4"));
-    items.push(SutotonItem::from("八分音符", "l8"));
-    items.push(SutotonItem::from("十六音符", "l16"));
-    items.push(SutotonItem::from("付点四分音符", "l4."));
-    items.push(SutotonItem::from("作者", "COPYRIGHT="));
-    items.push(SutotonItem::from("曲名", "TrackName="));
-    items.push(SutotonItem::from("コメント", "MetaText="));
-    items.push(SutotonItem::from("拍子", "TimeSig="));
-    items.push(SutotonItem::from("音源初期化", "ResetGM()"));
-    items.push(SutotonItem::from("演奏位置", "PlayFrom"));
-    items.push(SutotonItem::from("時間", "Time="));
-    items.push(SutotonItem::from("音長", "l"));
-    items.push(SutotonItem::from("音符", "l"));
-    items.push(SutotonItem::from("音階", "o"));
-    items.push(SutotonItem::from("音色", "@"));
-    items.push(SutotonItem::from("音量", "v"));
-    items.push(SutotonItem::from("音量予約", "v.onTime="));
-    items.push(SutotonItem::from("ゲート", "q"));
-    items.push(SutotonItem::from("連符", "Div"));
-    items.push(SutotonItem::from("読む", "Include"));
-    items.push(SutotonItem::from("ド", "c"));
-    items.push(SutotonItem::from("レ", "d"));
-    items.push(SutotonItem::from("ミ", "e"));
-    items.push(SutotonItem::from("ファ", "f"));
-    items.push(SutotonItem::from("フ", "f"));
-    items.push(SutotonItem::from("ソ", "g"));
-    items.push(SutotonItem::from("ラ", "a"));
-    items.push(SutotonItem::from("シ", "b"));
-    items.push(SutotonItem::from("ど", "c"));
-    items.push(SutotonItem::from("れ", "d"));
-    items.push(SutotonItem::from("み", "e"));
-    items.push(SutotonItem::from("ふぁ", "f"));
-    items.push(SutotonItem::from("ふ", "f"));
-    items.push(SutotonItem::from("そ", "g"));
-    items.push(SutotonItem::from("ら", "a"));
-    items.push(SutotonItem::from("し", "b"));
-    items.push(SutotonItem::from("ン", "r"));
-    items.push(SutotonItem::from("ッ", "r"));
-    items.push(SutotonItem::from("ー", "^"));
-    items.push(SutotonItem::from("「", "'"));
-    items.push(SutotonItem::from("」", "'"));
-    items.push(SutotonItem::from("【", "["));
-    items.push(SutotonItem::from("】", "]"));
-    items.push(SutotonItem::from("↑", ">"));
-    items.push(SutotonItem::from("↓", "<"));
-    items.push(SutotonItem::from("っ", "r"));
-    items.push(SutotonItem::from("ん", "r"));
-    items.push(SutotonItem::from("♭", "-"));
-    items.push(SutotonItem::from("♯", "#"));
-    items.push(SutotonItem::from("調", "KeyFlag="));
-    items.push(SutotonItem::from("ど", "n36,"));
-    items.push(SutotonItem::from("た", "n38,"));
-    items.push(SutotonItem::from("つ", "n42,"));
-    items.push(SutotonItem::from("く", "n44,"));
-    items.push(SutotonItem::from("ち", "n46,"));
-    items.push(SutotonItem::from("ぱ", "n49,"));
-    items.push(SutotonItem::from("−", "-"));
-    items.push(SutotonItem::from("‘", "`"));
-    items.push(SutotonItem::from("変", "-"));
-    items.push(SutotonItem::from("嬰", "#"));
-    items.push(SutotonItem::from("リズム", "RHYTHM"));
-    items.push(SutotonItem::from("イ", "a"));
-    items.push(SutotonItem::from("ロ", "b"));
-    items.push(SutotonItem::from("ハ", "c"));
-    items.push(SutotonItem::from("ニ", "d"));
-    items.push(SutotonItem::from("ホ", "e"));
-    items.push(SutotonItem::from("ヘ", "f"));
-    items.push(SutotonItem::from("ト", "g"));
+    items.set_item("全音符", "l1"); // 全音符を基本音符にする
+    items.set_item("二分音符", "l2"); // 二分音符を基本音符にする
+    items.set_item("四分音符", "l4"); // 四分音符を基本音符にする
+    items.set_item("八分音符", "l8"); // 八分音符を基本音符にする
+    items.set_item("十六音符", "l16"); // 十六音符を基本音符にする
+    items.set_item("付点四分音符", "l4."); // 付点四分音符を基本音符にする
+    items.set_item("音源初期化", "System.MeasureShift(1);ResetGM;Time(1:1:0);TrackSync;"); // @ 音源初期化//音源の初期化(GMリセット)を実行する。（例）音源初期化
+    items.set_item("音長", "l"); // 基本音符を指定
+    items.set_item("音量予約", "v.onTime="); // 音量を予約指定する
+    items.set_item("「", "'"); // @ 和音はじめ
+    items.set_item("」", "'"); // @ 和音おわり
+    items.set_item("【", "["); // @ 繰り返しはじめ
+    items.set_item("】", "]"); // @ 繰り返しおわり
+    items.set_item("↑", ">"); // @ オクターブを1つ上げる
+    items.set_item("↓", "<"); // @ オクターブを1つ下げる
+    items.set_item("♭", "-"); // @ フラット
+    items.set_item("♯", "#"); // @ シャープ
+    items.set_item("−", "-"); // @ マイナス
+    items.set_item("‘", "`"); // @ 次の音符をオクターブ1つ上げる
+    items.set_item("調", "System.KeyFlag"); // @ 調#(音符)//臨時記号を設定する。（例）調＃（ドファ）
+    items.set_item("音階", "o"); // @ 音階(数値)//音階を数値で指定する。初期値は５。範囲は、0～10（例）音階５
+    items.set_item("時間", "Time"); // @ 時間(小節数:拍数:ステップ数)//指定時間にポインタを移動する。範囲は、小節数・拍数が、１～。ステップ数は、０～。（例）時間（４：１：０）
+    items.set_item("読む", "Include"); // @ 読む(ファイル名)//外部定義ファイルを読み込む。（例）読む(chord2.h)
+    items.set_item("予約", ".onNote="); // @ (コマンド)予約(v1,v2,v3...)//コマンドの値を予約しておく（例）音量予約120,50【ドレミファ】
+    items.set_item("拍子", "System.TimeSignature="); // @ 拍子 分子,分母//拍子を設定する。（例）拍子4,4
+    items.set_item("音色", "@"); // @ 音色（番号）//音色を設定する。
+    items.set_item("音符", "l"); // @ 音符（ｎ分音符指定）//基本となる音符の長さをｎ分音符で指定する。（例）音符16//１６分音符の意味
+    items.set_item("音量", "v"); // @ 音量（数値）//音量(実際は音の強さ)を設定する。初期値は、100。範囲は、0~127。（例）音量127
+    items.set_item("連符", "Div"); // @ 連符{音名}[音長]//３連符や５連符などを表現する。（例）連符{ドレミ}4
+    items.set_item("ゲート", "q"); // @ ゲート（割合）//音符の長さに対する実際の発音時間を割合（100分率）で指定する。範囲は、1～100～。（例）ゲート80
+    items.set_item("テンポ", "Tempo="); // @ テンポ（数値）//テンポを設定する。初期値は、120。範囲は、20～240を推奨。（例）テンポ120
+    items.set_item("曖昧さ", ".Random="); // @ (コマンド)曖昧さ（数値）//各属性の曖昧さを設定する。範囲は、0～。初期値は、0。（例）音量曖昧さ80 【ドレミソ】
+    items.set_item("トラック", "Track="); // @ トラック（番号）//トラック番号を指定する。初期値は、０。範囲は、0～。（例）トラック３
+    items.set_item("チャンネル", "Channel="); // @ チャンネル（番号）//現在のトラックにチャンネルを設定する。初期値は、トラック番号と同じ。範囲は、１～１６（例）トラック３チャンネル１０
+    items.set_item("曲名", "TrackName="); // @ 曲名{"文字列"}//生成するMIDIファイルに曲名を埋め込む。（例）曲名{"テスト"}
+    items.set_item("作者", "Copyright="); // @ 作者{"文字列"}//生成するMIDIファイルに著作権情報を埋め込む。（例）作者{"クジラ飛行机"}
+    items.set_item("コメント", "MetaText="); // @ コメント{"文字列"}//生成するMIDIファイルにコメントを埋め込む。（例）コメント{"テスト"}
+    items.set_item("演奏位置", "PlayFrom"); // @ 演奏位置(小節数:拍数:ステップ数))//長い曲の途中から演奏したい時、曲の演奏位置を指定する。（例）演奏位置（32:1:0）
+    items.set_item("ー", "^"); // @ ー//タイ。音を伸ばす。（例）ドードレミミソーーー
+    items.set_item("上", ">"); // @ 音階を相対的に１つ上げる
+    items.set_item("下", "<"); // @ 音階を相対的に１つ下げる
+    items.set_item("ド", "c"); // @ 音名
+    items.set_item("レ", "d"); // @ 音名
+    items.set_item("ミ", "e"); // @ 音名
+    items.set_item("フ", "f"); // @ 音名
+    items.set_item("ァ", ""); // @ 音名
+    items.set_item("ソ", "g"); // @ 音名
+    items.set_item("ラ", "a"); // @ 音名
+    items.set_item("シ", "b"); // @ 音名
+    items.set_item("ン", "r"); // @ 休符。（例）ドーーン　レンレー
+    items.set_item("ッ", "r"); // @ 休符。（例）ドーーッ　レッレー
+    items.set_item("ど", "c"); // @ 音名
+    items.set_item("れ", "d"); // @ 音名
+    items.set_item("み", "e"); // @ 音名
+    items.set_item("ふ", "f"); // @ 音名
+    items.set_item("ぁ", ""); // @ 音名
+    items.set_item("そ", "g"); // @ 音名
+    items.set_item("ら", "a"); // @ 音名
+    items.set_item("し", "b"); // @ 音名
+    items.set_item("ん", "r"); // @ 休符
+    items.set_item("っ", "r"); // @ 休符
+    items.set_item("イ", "a"); // @ 音名
+    items.set_item("ロ", "b"); // @ 音名
+    items.set_item("ハ", "c"); // @ 音名
+    items.set_item("ニ", "d"); // @ 音名
+    items.set_item("ホ", "e"); // @ 音名
+    items.set_item("ヘ", "f"); // @ 音名
+    items.set_item("ト", "g"); // @ 音名
+    items.set_item("変", "-"); // @ フラット（例）イ変
+    items.set_item("嬰", "+"); // @ シャープ
+    items.set_item("リズム", "Rythm"); // @ リズムモード
+    items.set_item("ず", "n36,"); // @ バスドラム
+    items.set_item("た", "n38,"); // @ スネアドラム
+    items.set_item("つ", "n42,"); // @ ハイハット（クローズ）
+    items.set_item("ち", "n46,"); // @ ハイハット（オープン）
+    items.set_item("ぱ", "n49,"); // @ シンバル
+    items.set_item("と", "n50,"); // @ Lowタム
+    items.set_item("む", "n47,"); // @ Midタム
+    items.set_item("ろ", "n43,"); // @ Highタム
+    items.set_item("く", "n44,"); // @ ドラム
+    items.set_item("大きく", "Cresc="); // @ 大きく(音長),(最終値)//音量(EP)をだんだん大きくする
+    items.set_item("小さく", "Decresc="); // @ 小さく(音長),(最終値)//音量(EP)をだんだん小さくする
+    items.set_item("クレッシェンド", "Cresc="); // @ 大きく(音長),(最終値)//音量(EP)をだんだん大きくする
+    items.set_item("デクレッシェンド", "Cresc="); // @ 小さく(音長),(最終値)//音量(EP)をだんだん小さくする
+    items.set_item("音量戻す", "EP(127)"); // @ 音量(EP)を最大値に戻す
+    items.set_item("方向左", "P(0)"); // @ ステレオの左から音が出るようにする
+    items.set_item("方向左前", "P(32)"); // @ ステレオの左前から音が出るようにする
+    items.set_item("方向前", "P(64)"); // @ ステレオの前から音が出るようにする
+    items.set_item("方向右前", "P(96)"); // @ ステレオの右前から音が出るようにする
+    items.set_item("方向右", "P(127)"); // @ ステレオの右から音が出るようにする
+    items.set_item("方向回す", "P.onNoteWaveEx(0,127,!1,127,0,!1);"); // @ ステレオの左右を回す
+    items.set_item("ビブラートオフ", "M(0)"); // @ ビブラートをやめる
+    items.set_item("ペダル", "y64,127;"); // @ ペダルを踏む
+    items.set_item("放す", "y64,0;"); // @ ペダルを放す
+    items.set_item("テンポ改", "TempoChange="); // @ テンポ改([[[t1],t2],len])//テンポを推移的に変更する。lenを省略すると、全音符の間に推移し、t1を省略すると、以前の値からt2へ推移する。
+    items.set_item("ビブラート", "M.onNoteWaveEx(0,0,!4,0,96,!8);"); // @ 推移的なビブラートをかける
+    items.set_item("ここから演奏", "PlayFrom(Time);"); // @ 途中から演奏したいときに書く
     // </SUTOTON>
     items.sort_items();
     items
@@ -211,7 +231,7 @@ mod tests {
     #[test]
     fn test_basic() {
         assert_eq!(convert("ドレミ"), String::from("cde"));
-        assert_eq!(convert("トラック3ドレミ"), String::from("TR=3cde"));
+        assert_eq!(convert("トラック3ドレミ"), String::from("Track=3cde"));
     }
     #[test]
     fn test_ex() {
