94841f529cfa0502d9e47fc59cbcf00036e81797
kujirahand.mac2021.win11 <web@kujirahand.com>
2024-07-19 09:23:32 +0900

    add windows build bat

---
+++ build_bin.bat
@@ -0,0 +1,10 @@
+set TARGET_DIR=.\sakuramml-bin
+
+cargo build --release
+# copy
+mkdir %TARGET_DIR%
+copy README.md %TARGET_DIR%\
+copy README_ja.md %TARGET_DIR%\
+copy target\release\sakuramml.exe %TARGET_DIR%\
+
+echo "OK"
